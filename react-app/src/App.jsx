1a73c08246df83de899ca1cc4b2e0c6a36ad6ae1
Pipsy <gpflaslik7112@gmail.com>
2026-05-14 20:31:07 +0300

    chore: refine App component

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -4,14 +4,3 @@ import TransactionList from './components/TransactionList.jsx';
 import SpendingChart from './components/SpendingChart.jsx';
 import './App.css';
 
-import { applyTax, calculateTotals, categorizeData, format_kenya_shillings,} from './utils/transactionUtils.js';
-
-
-
-// Storage key for saving finance data in local machine
-
-const savedFinanceDataKey = 'saved-finanace-data';
-
-
- 
-
